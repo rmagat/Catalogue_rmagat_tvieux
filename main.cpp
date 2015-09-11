8b15ced2dce3587f311b713c079dfcc1e4e15eeb
rmagat <rmagat@f005ThinkStation13.btsinfogap.org>
2015-09-11 16:23:37 +0200

    commentaires du main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+/**
+ * @author robin magat
+ * @file pdf.cpp
+ * @date 11/09/2015
+ * @brief toutes les fonctions et constructeurs
+ */
+
 #include <QApplication>
 #include "mainwindow.h"
 
+/**
+ * @brief main
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
