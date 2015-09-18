20da014aea2ec97750a8e34bccd4247379ca4065
VieuxThomas <thomasvieux04300@gmail.com>
2015-09-18 13:30:28 +0200

    ajout de la base de données

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+#include "pdf.h"
+#include <QMessageBox>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QSqlRecord>
+#include <QSqlRelationalDelegate>
 /**
  * @author robin magat
  * @file pdf.cpp
@@ -16,6 +22,18 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    maBase=new QSqlDatabase(db);
+    maBase->setHostName("localhost");
+         maBase->setDatabaseName("NewWorld");
+         maBase->setUserName("tvieux");
+         maBase->setPassword("ini01");
+         bool ok = maBase->open();
+         if(!ok)
+         {
+             QMessageBox::warning(this,"New World BackOffice","la connexion a la base de donnée a echouer \n Veuillez verifier que le service mysql est lancer sur localhost",QMessageBox::Ok|QMessageBox::Cancel,QMessageBox::Ok);
+         }
+
     
     return a.exec();
 }
